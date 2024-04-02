f5b87b291939dd8d7d5ea58cd25b6a964cd047bb
Bascojomar <113278770+Bascojomar@users.noreply.github.com>
2024-04-02 18:39:54 +0800

    ifelse
    records

--- index1.php
+++ index1.php
@@ -123,6 +123,7 @@ margin-top: 10%;
 	<?php 
 		include 'database.php'; 
 		session_start();
+		
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$username = $_POST['username'];
 			$password = $_POST['password'];
--- records/dashboard.php
+++ records/dashboard.php
@@ -2,6 +2,50 @@
 include '../database.php';
 include '../session.php';
 
+$office = $_SESSION['Office'];
+
+if ($office == 'SITE ADMIN' ){
+  header('location: ../admin/admin');
+}
+elseif($office == 'VPAA'){
+  header('location: ../vpaa/vpaa');
+}
+elseif($office == 'VPAA1'){
+  header('location: ../vpaa/dashboard');
+}
+elseif($office == 'VPAA2'){
+  header('location: ../vpaa/dashboard');
+}
+elseif($office == 'VPABM'){
+  header('location: ../vpaa/vpaa');
+}
+elseif($office == 'VPABM1'){
+  header('location: ../vpabm/dashboard');
+}
+elseif($office == 'VPABM2'){
+  header('location: ../vpabm/dashboard');
+}
+elseif($office == 'VPRET'){
+  header('location: ../vpaa/vpaa');
+}
+elseif($office == 'VPRET'){
+  header('location: ../vpret/dashboard');
+}
+elseif($office == 'VPRET'){
+  header('location: ../vpret/dashboard');
+}
+elseif($office == 'PRESIDENT'){
+  header('location: ../vpaa/vpaa');
+}
+elseif($office == 'PRESIDENT'){
+  header('location: ../president/dashboard');
+}
+elseif($office == 'PRESIDENT'){
+  header('location: ../president/dashboard');
+}
+elseif($office == 'PROCUREMENT'){
+  header('location: ../produrement/dashboard');
+}
 
 $sql = "SELECT * FROM doctrack.tbl_users WHERE Office = 'RECORDS'";
 $result = $conn->query($sql);

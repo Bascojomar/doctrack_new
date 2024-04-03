2973137b2102881571d7aeeb0ae3245eface2316
Bascojomar <113278770+Bascojomar@users.noreply.github.com>
2024-04-03 13:54:44 +0800

    send_select
    send_select

---
+++ records/send_select_campus.php
@@ -0,0 +1,17 @@
+<?php
+include 'database.php';
+$campus_id = $_GET['campus_id'];
+
+
+
+  echo"<SELECT name='fromoffice' id='fromoffice' class='forminput'>";
+  echo "<OPTION value = '' disabled selected hidden>Select Office</OPTION>";
+    $rs = mysqli_query($conn,'SELECT dept_id, dept_name, campus_id 
+                from tbl_department WHERE campus_id='.$campus_id.' order by dept_name');
+    while($rw = mysqli_fetch_array($rs)){
+        echo'<option value="'.$rw['dept_name'].'">'.$rw['dept_name'].'</option>';
+    }
+    
+  echo "</SELECT>";
+?>
+

f6c44f5701c5cc16c96c13ac8f8469bf1388c5cf
doctrack01 <165653612+doctrack01@users.noreply.github.com>
2024-04-08 09:43:48 +0800

    records
    records

--- vpaa/vpaa.php
+++ vpaa/vpaa.php
@@ -22,26 +22,19 @@ elseif($office == 'VPAA1'){
 elseif($office == 'VPAA2'){
   header('location: ../vpaa/dashboard');
 }
-elseif($office == 'VPABM'){
-  header('location: ../vpaa/vpaa');
-}
+
 elseif($office == 'VPABM1'){
   header('location: ../vpabm/dashboard');
 }
 elseif($office == 'VPABM2'){
   header('location: ../vpabm/dashboard');
 }
-elseif($office == 'VPRET'){
-  header('location: ../vpaa/vpaa');
-}
+
 elseif($office == 'VPRET1'){
   header('location: ../vpret/dashboard');
 }
 elseif($office == 'VPRET2'){
   header('location: ../vpret/dashboard');
-}
-elseif($office == 'PRESIDENT'){
-  header('location: ../vpaa/vpaa');
 }
 elseif($office == 'PRESIDENT1'){
   header('location: ../president/dashboard');

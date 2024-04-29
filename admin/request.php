d719e89baf286055c2dc1431cb0621e581fd599f
Bascojomar <113278770+Bascojomar@users.noreply.github.com>
2024-04-29 21:25:41 +0800

    approved
    approved

--- admin/request.php
+++ admin/request.php
@@ -258,7 +258,7 @@ while ($row = $result->fetch_assoc()) {
                                         echo "<td>" . $rows["Email"] . "</td>";
                                         echo "<td>" . $rows["UserName"] . "</td>";
                                         echo "<td>" . $rows["Request"] . $rows["Letter"] . "</td>";
-                                        echo "<td><form action='sendreq' method='POST' class='text-center'><input type='hidden' name='users' value='" . $rows["UserName"] . "'><input type='hidden' name='pass' value='" . $rows["Password"] . "'><input type='hidden' name='camp' value='" . $rows["Campus"] . "'><input type='hidden' name='email' value='" . $rows["Email"] . "'><input type='hidden' name='unique_id' value='" . $rows["ID"] . "'><button type='submit' class='btn btn-primary' name='approved' value='Approved'><i class='bi bi-hand-thumbs-up' style='font-size:22px;'></i> Approved</button></form></td>";
+                                        echo "<td><form action='' method='POST' class='text-center'><input type='hidden' name='users' value='" . $rows["UserName"] . "'><input type='hidden' name='pass' value='" . $rows["Password"] . "'><input type='hidden' name='camp' value='" . $rows["Campus"] . "'><input type='hidden' name='email' value='" . $rows["Email"] . "'><input type='hidden' name='id' value='" . $rows["ID"] . "'><button type='submit' class='btn btn-primary' name='approved' value='Approved'><i class='bi bi-hand-thumbs-up' style='font-size:22px;'></i> Approved</button></form></td>";
                                         echo "</tr>";
                                     }
                                 }
@@ -267,6 +267,113 @@ while ($row = $result->fetch_assoc()) {
                     </div>
                 </div>';
             }
+
+            // sending aprroved
+
+            
+// send password and username
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/autoload.php';
+
+// Assuming you have a database connection established in $conn
+
+if (isset($_POST['approved'])) {
+  $user_id_to_delete = $_POST['id'];
+
+  // Display a confirmation dialog using SweetAlert
+  echo "<script>
+          document.addEventListener('DOMContentLoaded', function() {
+              var sweetAlertScript = document.createElement('script');
+              sweetAlertScript.src = 'https://unpkg.com/sweetalert/dist/sweetalert.min.js';
+              document.head.appendChild(sweetAlertScript);
+              
+              sweetAlertScript.onload = function() {
+                  swal({
+                      title: 'Are you sure?',
+                      text:'You want to send this username and password?',
+                      icon: 'warning',
+                      buttons: true,
+                      dangerMode: true,
+                  })
+                  .then((willDelete) => {
+                      if (willDelete) {
+                          window.location.href = 'request?id=$user_id_to_delete&confirmed=1';
+                      } else {
+                          window.location.href = 'request';
+                      }
+                  });
+              };
+          });
+        </script>";
+
+}
+
+
+// Check for confirmation parameter and perform the deletion
+if (isset($_GET['confirmed']) && $_GET['confirmed'] == 1) {
+    $user_id_to_delete = $_GET['id'];
+    
+        $sql = "SELECT * FROM tbl_users WHERE ID = '$user_id_to_delete'";
+        $result = $conn->query($sql);
+    
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $user = $row['UserName'];
+            $pass = $row['Password'];
+            $uniqueId = $row['ID'];
+            $email = $row['Email'];
+            
+            $subject = "Document Status Update in campus";
+            $message = "Greetings campus<br>
+            Your document Username : $user <br> Password : $pass.";
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'docutracking01@gmail.com';
+            $mail->Password = 'jiejyzzhrhpjltug';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+            
+            $mail->setFrom('docutracking01@gmail.com');
+            $mail->addAddress($email);
+            $mail->isHTML(true);
+            
+            $mail->Subject = $subject;
+            $mail->Body = $message;
+            
+                $mail->send();
+            
+            
+            $updateQuery = "UPDATE tbl_users SET Letter = '', Request = '' WHERE ID = $uniqueId";
+            $conn->query($updateQuery);
+           
+    
+            echo '<script>
+            document.addEventListener("DOMContentLoaded", function() {
+              var sweetAlertScript = document.createElement("script");
+              sweetAlertScript.src = "https://unpkg.com/sweetalert/dist/sweetalert.min.js";
+              document.head.appendChild(sweetAlertScript);
+            
+              sweetAlertScript.onload = function() {
+                swal({
+                  title: "Successful Sending Password",
+                  text: "Send",
+                  icon: "success",
+                  buttons: false,
+                  timer: 1200
+                }).then(function() {
+                  window.location.href = "request";
+                });
+              };
+            });
+            </script>';
+            
+            
+    }
+    }
         ?>
     </main>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- admin/sendreq.php
+++ admin/sendreq.php
@@ -1,58 +1,108 @@
 <?php
 include '../database.php';
 
+// send password and username
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require '../vendor/autoload.php';
 
+// Assuming you have a database connection established in $conn
 
-$user = $_POST['users'];
-$pass = $_POST['pass'];
-$email = $_POST['email'];
-$uniqueId = $_POST['unique_id'];
+if (isset($_POST['approved'])) {
+  $user_id_to_delete = $_POST['id'];
 
-$subject = "Document Status Update in campus";
-$message = "Greetings campus<br>
-Your document Username : $user <br> Password : $pass.";
-$mail = new PHPMailer(true);
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'docutracking01@gmail.com';
-$mail->Password = 'jiejyzzhrhpjltug';
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
+  // Display a confirmation dialog using SweetAlert
+  echo "<script>
+          document.addEventListener('DOMContentLoaded', function() {
+              var sweetAlertScript = document.createElement('script');
+              sweetAlertScript.src = 'https://unpkg.com/sweetalert/dist/sweetalert.min.js';
+              document.head.appendChild(sweetAlertScript);
+              
+              sweetAlertScript.onload = function() {
+                  swal({
+                      title: 'Are you sure?',
+                      text:'You want to send this username and password?',
+                      icon: 'warning',
+                      buttons: true,
+                      dangerMode: true,
+                  })
+                  .then((willDelete) => {
+                      if (willDelete) {
+                          window.location.href = 'sendreq?id=$user_id_to_delete&confirmed=1';
+                      } else {
+                          window.location.href = 'request';
+                      }
+                  });
+              };
+          });
+        </script>";
 
-$mail->setFrom('docutracking01@gmail.com');
-$mail->addAddress($email);
-$mail->isHTML(true);
-
-$mail->Subject = $subject;
-$mail->Body = $message;
-
-    $mail->send();
+}
 
 
-$updateQuery = "UPDATE tbl_users SET Letter = '', Request = '' WHERE ID = $uniqueId";
-$conn->query($updateQuery);
-echo '<script>
-document.addEventListener("DOMContentLoaded", function() {
-  var sweetAlertScript = document.createElement("script");
-  sweetAlertScript.src = "https://unpkg.com/sweetalert/dist/sweetalert.min.js";
-  document.head.appendChild(sweetAlertScript);
+// Check for confirmation parameter and perform the deletion
+if (isset($_GET['confirmed']) && $_GET['confirmed'] == 1) {
+    $user_id_to_delete = $_GET['id'];
+    
+        $sql = "SELECT * FROM tbl_users WHERE ID = '$user_id_to_delete'";
+        $result = $conn->query($sql);
+    
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $user = $row['UserName'];
+            $pass = $row['Password'];
+            $uniqueId = $row['ID'];
+            $email = $row['Email'];
+            
+            $subject = "Document Status Update in campus";
+            $message = "Greetings campus<br>
+            Your document Username : $user <br> Password : $pass.";
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'docutracking01@gmail.com';
+            $mail->Password = 'jiejyzzhrhpjltug';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+            
+            $mail->setFrom('docutracking01@gmail.com');
+            $mail->addAddress($email);
+            $mail->isHTML(true);
+            
+            $mail->Subject = $subject;
+            $mail->Body = $message;
+            
+                $mail->send();
+            
+            
+            $updateQuery = "UPDATE tbl_users SET Letter = '', Request = '' WHERE ID = $uniqueId";
+            $conn->query($updateQuery);
+           
+            echo '<script>
+            document.addEventListener("DOMContentLoaded", function() {
+              var sweetAlertScript = document.createElement("script");
+              sweetAlertScript.src = "https://unpkg.com/sweetalert/dist/sweetalert.min.js";
+              document.head.appendChild(sweetAlertScript);
+            
+              sweetAlertScript.onload = function() {
+                swal({
+                  title: "Successful Sending Password",
+                  text: "Send",
+                  icon: "success",
+                  buttons: false,
+                  timer: 1200
+                }).then(function() {
+                  window.location.href = "request";
+                });
+              };
+            });
+            </script>';
+            
+            
+    }
+    }
 
-  sweetAlertScript.onload = function() {
-    swal({
-      title: "Update Successful",
-      text: "Successful",
-      icon: "success",
-      buttons: false,
-      timer: 1200
-    }).then(function() {
-      window.location.href = "request";
-    });
-  };
-});
-</script>';
+
 ?>
